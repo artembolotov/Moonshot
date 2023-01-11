4bca7fbc6ea4e713028d3af1956714c8483b709b
Artem Bolotov <teeemon@gmail.com>
2023-01-11 08:01:08 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by artembolotov on 11.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
